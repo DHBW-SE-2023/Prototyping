1b5355475a1628f4669b3c78b13b737cddfdd48f
MaxAlberti <maxalberti@gmx.de>
2023-10-29 13:00:26 +0100

    Finalized Prototype

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,22 @@
 use rocket::{fs::NamedFile, response::Redirect};
 use std::path::{Path, PathBuf};
 use rocket::form::Form;
+use rocket::form::FromForm;
+use rocket::post;
 
 #[derive(FromForm)]
 struct FormData {
-    result: String,
-    id: String,
+    txt: String,
 }
 
 #[post("/home", data = "<form_data>")]
 fn submit(form_data: Form<FormData>) -> String {
     let data = form_data.into_inner();
-    format!("Received form data: id = {}", data.id)
+    if data.txt.to_lowercase() == "hello" {
+        format!("Hi!")
+    } else {
+        format!("Rude!")
+    }
 }
 
 #[get("/")]
@@ -36,5 +41,5 @@ async fn files(file: PathBuf) -> Option<NamedFile> {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, home, files])
+    rocket::build().mount("/", routes![index, home, files, submit])
 }
